8ad6799dd9de080ddfd2b60800a790e1aecc6f2e
tungbt94 <tungbt94@gmail.com>
2016-05-12 12:14:35 +0700

    Add get_list_staff_voted and get_sum_score to option

--- app/models/option.rb
+++ app/models/option.rb
@@ -2,8 +2,21 @@ class Option < ActiveRecord::Base
   belongs_to :poll
 
   def get_list_staff_voted
+    list_staff_poll = StaffPoll.where(:option_id => self.id)
+    list_staff = Array.new
+    @sum_score = 0
+    staff = Staff.new
+    list_staff_poll.each do |staff_poll|
+      staff_id = staff_poll.staff_id
+      staff = Staff.find(staff_id)
+      list_staff.push(staff)
+      @sum_score += staff.get_score
+    end
+    return list_staff
   end
 
   def get_sum_score
+    get_list_staff_voted
+    return @sum_score
   end
 end
--- app/models/poll.rb
+++ app/models/poll.rb
@@ -3,6 +3,13 @@ class Poll < ActiveRecord::Base
   has_many :option
 
   def get_list_staff_voted
+    list_staff_poll = StaffPoll.where(:poll_id => self.id)
+    list_staff = Array.new
+    list_staff_poll.each do |staff_poll|
+      staff_id = staff_poll.staff_id
+      list_staff.push(Staff.find(staff_id))
+    end
+    return list_staff
   end
 
 end
