c2479d14bff57dc8bbb580e0c633167b11ff1397
tuandh <tuandh@gmail.com>
2016-05-12 14:29:45 +0700

    Change domain email

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -4,7 +4,7 @@ class Staff < ActiveRecord::Base
   				end
 				}
   validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@vccloud.vn/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@vcc.com/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }, allow_nil: true
